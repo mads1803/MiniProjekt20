bca761baf7a8aa4833d3b5d635876a8082ad59d3
MadsS <stoltenborg@hotmail.dk>
2018-03-15 10:25:08 +0100

    Merge

--- app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/MainActivity.java
+++ app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.madsstoltenborg.miniprojektforfra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +15,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> listView,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this,
+                                    ShopListActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
 
-
-
-        //asdkadsdsadas
+        // Initialize DatabaseHelper
+        ShoppingDatabaseHelper.setApplicationContext(this);
     }
 
 
