05acc7be4a31ecabb67801f7e7dbfe80ee8849c6
MadsS <stoltenborg@hotmail.dk>
2018-03-15 13:00:33 +0100

    Product class oprettet

---
+++ app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/Product.java
@@ -0,0 +1,9 @@
+package com.example.madsstoltenborg.miniprojektforfra;
+
+/**
+ * Created by Mads Stoltenborg on 15-03-2018.
+ */
+
+public class Product {
+    
+}
--- app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/ProductCursorWrapper.java
+++ app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/ProductCursorWrapper.java
@@ -1,12 +1,13 @@
 package com.example.madsstoltenborg.miniprojektforfra;
 
 import android.database.Cursor;
+import android.database.CursorWrapper;
 
 /**
  * Created by Mads Stoltenborg on 15-03-2018.
  */
 
-public class ProductCursorWrapper {
+public class ProductCursorWrapper extends CursorWrapper {
     public ProductCursorWrapper(Cursor cursor){
         super(cursor);
     }
@@ -14,8 +15,8 @@ public class ProductCursorWrapper {
     public Shop getProduct(){
         long id = getLong(getColumnIndex("_id"));
         String name = getString(getColumnIndex("NAME"));
-        String address = getString(getColumnIndex("ADDRESS"));
-        String website = getString(getColumnIndex("WEBSITE"));
-        return new Product(id, name, address, website);
+        String address = getString(getColumnIndex("PRICE"));
+        long shopid = getLong(getColumnIndex("SHOPID"));
+        return new Product(id, name, address, shopid);
     }
 }
