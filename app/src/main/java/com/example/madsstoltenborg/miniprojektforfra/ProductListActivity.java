e17e96b42219f8bc94ad16cb0f8dcefa113f2624
Simon Tofteby <simontofteby@gmail.com>
2018-03-16 14:50:23 +0100

    alertfragment to add products

--- app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/ProductListActivity.java
+++ app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/ProductListActivity.java
@@ -1,16 +1,24 @@
 package com.example.madsstoltenborg.miniprojektforfra;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Mads Stoltenborg on 15-03-2018.
@@ -58,9 +66,7 @@ public class ProductListActivity extends AppCompatActivity {
                 return true;
 
             case R.id.action_Addproduct:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-
+              showAddProductDialog();
                 return true;
             case R.id.action_deleteProduct:
                 // User chose the "Favorite" action, mark the current item
@@ -74,4 +80,44 @@ public class ProductListActivity extends AppCompatActivity {
 
         }
     }
-}
+
+    private void showAddProductDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.custom_dialog_products, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText name = (EditText) dialogView.findViewById(R.id.dialogProductName);
+        final EditText volume = (EditText) dialogView.findViewById(R.id.dialogProductVolume);
+
+
+        dialogBuilder.setTitle("Add Product");
+        dialogBuilder.setMessage("Enter text below");
+        dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            Storage.insertProduct(name.getText().toString(),volume.getText().toString());
+                //do something with edt.getText().toString();
+                Context context = getApplicationContext();
+                CharSequence text = "You have added a product!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+              Context context = getApplicationContext();
+                CharSequence text = "You have canceled!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+        }
+    }
+
+
