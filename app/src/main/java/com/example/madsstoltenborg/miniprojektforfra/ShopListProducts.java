6a095d569f3c9c69e5e5931d36eb631a5da8b5e5
MadsS <stoltenborg@hotmail.dk>
2018-03-18 23:11:40 +0100

    Tilføjet spinner (Dropdownmenu så man kan vælge en indkøbsliste at tilføje produkter til) Data is spinner er hardcoded

--- app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/ShopListProducts.java
+++ app/src/main/java/com/example/madsstoltenborg/miniprojektforfra/ShopListProducts.java
@@ -5,11 +5,16 @@ import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class ShopListProducts extends AppCompatActivity {
     public static final String EXTRA_SHOPID = "shopId";
@@ -31,6 +36,18 @@ public class ShopListProducts extends AppCompatActivity {
             name.setText(shop.getName());
 
             ListView listProducts = (ListView) findViewById(R.id.listShopProducts);
+
+
+
+
+            Spinner spinner = (Spinner) findViewById(R.id.listDropDown);
+// Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.spinnerTestData, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+            spinner.setAdapter(adapter);
 
             try {
                 //TODO: GetShopProducts skal rettes og bruges i stedet for getProducts
